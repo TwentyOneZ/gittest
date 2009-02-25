3fd1f8ae02eff36fd535c59d676839d754c9a5a2
Magus <magishan.magus@gmail.com>
2009-02-25 20:08:29 -0300

    Created motorcycle.rb

--- motorcycle.rb
+++ motorcycle.rb
@@ -1,15 +1,15 @@
 class MotorCycle
-  def initialize(make, color)
-    # Instance variables
-    @make = make
-    @color = color
-  end
-  def startEngine
-    if (@engineState)
-      puts 'Engine is already Running'
-    else
-      @engineState = true
-      puts 'Engine Idle'
-    end
-  end
+def initialize(make, color)
+# Instance variables
+@make = make
+@color = color
+end
+def startEngine
+if (@engineState)
+puts 'Engine is already Running'
+else
+@engineState = true
+puts 'Engine Idle'
+end
+end
 end
